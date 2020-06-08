74aba6929d4c72966c5469b818f21e0cf89625e3
gitlost-murali <kmanoharmurali@gmail.com>
2020-06-08 20:59:59 +0530

    Audio colors for predictions added

--- Flask_Website/server.py
+++ Flask_Website/server.py
@@ -5,13 +5,28 @@ from pathlib import Path
 
 app = Flask(__name__)
 
-indexnums=[]
-predictions = {}
+
+def read_predsdata(predfolder='static/preds_data'):
+    predfiles = glob(f'{predfolder}/*')
+    indexnums=[]
+    predictions = {}
+    urls = {}
+    for predfile in predfiles:
+        indexno = Path(predfile).stem
+        indexnums.append(int(indexno))
+        with open(f'{predfile}','r') as fh:
+            jsondata = json.load(fh)
+        
+        urls[indexno]=(jsondata['data']['url'])
+        for entry in jsondata['completions'][0]['result']:
+            predictions[indexno] = predictions.get(indexno,[]) + [entry['value']]
+            # print(entry['value'])
+    return sorted(indexnums), predictions, urls
+
+indexnums, predictions, urls = read_predsdata()
 
 @app.route('/')
 def home():
-    indexnums, predictions = read_predsdata()
-    print(indexnums)
     return render_template('index.html',data=indexnums)
 
 @app.route('/sentiment')
@@ -19,21 +34,11 @@ def sentiment():
     indexno = int(request.args.get('indexno'))
     return render_template('sentiment.html',data=indexno)
 
-def read_predsdata(predfolder='static/preds_data'):
-    predfiles = glob(f'{predfolder}/*')
-    indexnums=[]
-    predictions = {}
-    for predfile in predfiles:
-        indexno = Path(predfile).stem
-        indexnums.append(int(indexno))
-        with open(f'{predfile}','r') as fh:
-            jsondata = json.load(fh)
+@app.route('/getdata',methods=['POST','GET'])
+def getdata():
+    index = request.args.get('indexno')
+    print(predictions[index][-1])
+    return jsonify({'preds':predictions[index],'length':predictions[index][-1]['end'],'url':urls[index],'vidurl':urls[index].replace("_bgm","").replace('/mediafiles/','/videofiles/').replace('.mp3','.mp4')})
 
-        for entry in jsondata['completions'][0]['result']:
-            predictions[indexno] = predictions.get(indexno,[]) + [entry['value']] 
-            # print(entry['value'])
-    return sorted(indexnums), predictions
 if __name__=='__main__':
     app.run(debug=True,port=7000)
-    indexnums=[]
-    predictions = {}
